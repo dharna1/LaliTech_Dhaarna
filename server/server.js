45fecc7f0d111e05ba66c9396705e5e173842aa9
Dharna <champbehal@gmail.com>
2026-06-15 08:01:57 +0000

    Fix MongoDB connection

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ process.on('unhandlesRejection', (err) => {
 });
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'; 
-//import  connectDB  from './config/db.js';
+import 'dotenv/config';
+import  connectDB  from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 
-//connectDB();
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
